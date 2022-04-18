dc39ca26c2a102f389e4a2ad1dacd095f8f2280a
jbhernin <jbhernin@hotmail.com>
2022-04-18 14:31:51 +0200

    creation du fichier app.js et import de ce fichier dans server.js

---
+++ src/backend/app.js
@@ -0,0 +1,7 @@
+//import de l'application Express
+const express = require('express')
+//creation de la constante app qui 'appelle' la méthode express
+const app = express();
+
+//export de la methode express afin de s'en servir dans d'autres fichiers
+module.exports = app;
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,10 @@
 // import du package HTTP
 const http = require('http');
+// on importe le fichier app.js
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('serveur opérationnel !');
-});
+
+const server = http.createServer(app);
 // le serveur est en écoute sur le port de l'environnement systeme 
 // ou sinon le port 3000 par défaut
 server.listen(process.env.PORT || 3000);
